486366f73b04b19cd4d5dd2c099c061c5cfa9c47
Randall Lunetto <rlunetto@gmail.com>
2020-11-09 12:00:36 -0300

    Create Test Sqoop Analytics Engine and MySQL Database.sh

---
+++ Test Sqoop Analytics Engine and MySQL Database.sh
@@ -0,0 +1,19 @@
+# Connect to MySQL DB (ClearDB) and review database tables to be loaded into Hadoop
+
+mysql -h bdaicdb.xxx.com -P 3306 -u hadoop -pcisbigdata
+
+# In MySQL shell
+
+show databases;
+
+use movielens;
+
+show tables;
+
+# Review the structure and contents of the movie table with SQL
+
+DESCRIBE movie;
+
+SELECT * FROM movie LIMIT 5;
+ 
+
